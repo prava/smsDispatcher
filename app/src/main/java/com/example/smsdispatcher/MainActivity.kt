6a5ba596b4c687650d9abd581e3f8e1e7e52afd7
levan.zurabashvili <l.zurabashvili@gmail.com>
2020-01-31 18:18:32 +0400

    showMesage-ს გადაეცემა callback ფუნქცია OK-ისთვის

--- app/src/main/java/com/example/smsdispatcher/MainActivity.kt
+++ app/src/main/java/com/example/smsdispatcher/MainActivity.kt
@@ -1,52 +1,65 @@
 package com.example.smsdispatcher
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "smsdispatcherTag"
+    private val SMS_READ_PERMISSION_CODE = 100
+    private val SMS_RECAVE_PERMISSION_CODE = 101
+    private val INTERNET_PERMISSION_CODE = 102
+    private val READ_STORAGE_PERMISSION_CODE = 103
+    private val WRITE_STORAGE_PERMISSION_CODE = 104
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        smsDispatcherSwitch.isEnabled = false;
 
         smsListenerSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
-                Log.i(TAG,"Switch on");
+                Log.i(TAG,"smsListenerSwitch Switch on");
             } else {
-                Log.i(TAG,"Switch off");
+                Log.i(TAG,"smsListenerSwitch Switch off");
             }
         }
 
         smsDeliverySwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
-                if(!smsListenerSwitch.isChecked)
+                if(!smsListenerSwitch.isChecked) {
                     smsListenerSwitch.isChecked = true;
-                Log.i(TAG,"Switch on");
+                }
+                Log.i(TAG,"smsDeliverySwitch Switch on");
             } else {
-                Log.i(TAG,"Switch off");
+                Log.i(TAG,"smsDeliverySwitch Switch off");
             }
         }
 
         smsDispatcherSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
-                Log.i(TAG,"Switch on");
+                Log.i(TAG,"smsDispatcherSwitch Switch on");
             } else {
-                Log.i(TAG,"Switch off");
+                Log.i(TAG,"smsDispatcherSwitch Switch off");
             }
         }
 
         setUrlButton.setOnClickListener {
+            Toast.makeText(this, "click", Toast.LENGTH_LONG)
             setUrl();
         }
 
@@ -64,18 +77,29 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun showMesage(ttl: String, txt: String) {
-        val alert = AlertDialog.Builder(this);
-        alert.setTitle(ttl).setMessage(txt).setPositiveButton("OK"){
-            dialog, which -> {
+    private  fun setupPermissions() {
+        makeRequest(Manifest.permission.INTERNET, INTERNET_PERMISSION_CODE)
+        makeRequest(Manifest.permission.READ_SMS, SMS_READ_PERMISSION_CODE)
+        makeRequest(Manifest.permission.RECEIVE_SMS, SMS_RECAVE_PERMISSION_CODE)
+        makeRequest(Manifest.permission.READ_EXTERNAL_STORAGE, READ_STORAGE_PERMISSION_CODE)
+        makeRequest(Manifest.permission.WRITE_EXTERNAL_STORAGE, WRITE_STORAGE_PERMISSION_CODE)
+    }
 
-        }
-        }.create().show();
+    private fun checkPermition(permitionTyoe: String): Int {
+        return ContextCompat.checkSelfPermission(this, permitionTyoe)
+    }
+
+    private fun makeRequest(permitionTyoe: String, PermitionCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permitionTyoe), PermitionCode)
+    }
+
+    private fun showMesage(ttl: String, txt: String, callback: (() -> Unit?)?) {
+        AlertDialog.Builder(this).setTitle(ttl).setMessage(txt).setPositiveButton("OK"){ dialog, which -> if(callback != null) callback();} .create().show();
     }
 
     private fun setUrl() {
         if(urlEditText.text.isEmpty()) {
-            showMesage("Error", "Enter dispatch URL")
+            //showMesage("Error", "Enter dispatch URL")
         }
         else
         {
@@ -96,3 +120,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
